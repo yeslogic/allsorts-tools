a12ba6e4275101ff1be69977a329cea989ae2f00
Wesley Moore <wes@wezm.net>
2019-04-09 11:00:30 +1000

    Add support for dumping individual table to dump tool

--- dump.rs
+++ dump.rs
@@ -1,4 +1,7 @@
+use atty::Stream;
 use encoding_rs::{Encoding, MACINTOSH, UTF_16BE};
+use getopts::Options;
+
 use fontcode::error::ParseError;
 use fontcode::fontfile::FontFile;
 use fontcode::read::ReadScope;
@@ -6,33 +9,72 @@ use fontcode::tables::{HeadTable, MaxpTable, NameTable, OffsetTable, OpenTypeFon
 use fontcode::tag;
 use fontcode::woff::WoffFile;
 use fontcode::woff2::{Woff2File, Woff2GlyfTable, Woff2LocaTable};
-use std::env;
+
 use std::fmt;
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
 use std::str;
+use std::{env, process};
+
+type Tag = u32;
 
 fn main() -> Result<(), ParseError> {
     let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
 
-    if args.len() != 2 {
-        println!("Usage: dump FILE");
+    let mut opts = Options::new();
+    opts.optopt("t", "table", "dump the content of this table", "TABLE");
+    opts.optflag("h", "help", "print this help menu");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
         return Ok(());
     }
 
-    let filename = &args[1];
+    let table = matches.opt_str("t").map(|table| {
+        let mut tag = [0u8; 4];
+        for (i, byte) in table.bytes().take(4).enumerate() {
+            tag[i] = byte;
+        }
+        tag::tag(tag)
+    });
+
+    if table.is_some() && atty::is(Stream::Stdout) {
+        eprintln!("Not printing binary data to tty.");
+        process::exit(1);
+    }
+
+    let filename = if !matches.free.is_empty() {
+        matches.free[0].as_str()
+    } else {
+        print_usage(&program, opts);
+        return Ok(());
+    };
+
     let buffer = read_file(filename)?;
 
     match ReadScope::new(&buffer).read::<FontFile>()? {
         FontFile::OpenType(font_file) => match font_file.font {
-            OpenTypeFont::Single(ttf) => dump_ttf(font_file.scope, ttf)?,
-            OpenTypeFont::Collection(ttc) => dump_ttc(font_file.scope, ttc)?,
+            OpenTypeFont::Single(ttf) => dump_ttf(font_file.scope, ttf, table)?,
+            OpenTypeFont::Collection(ttc) => dump_ttc(font_file.scope, ttc, table)?,
         },
-        FontFile::Woff(woff_file) => dump_woff(woff_file.scope, woff_file)?,
-        FontFile::Woff2(woff_file) => dump_woff2(woff_file.table_data_block_scope(), &woff_file)?,
+        FontFile::Woff(woff_file) => dump_woff(woff_file.scope, woff_file, table)?,
+        FontFile::Woff2(woff_file) => {
+            dump_woff2(woff_file.table_data_block_scope(), &woff_file, table)?
+        }
     }
 
     Ok(())
+}
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options] FONTFILE ", program);
+    eprint!("{}", opts.usage(&brief));
 }
 
 fn read_file(path: &str) -> Result<Vec<u8>, io::Error> {
@@ -42,7 +84,11 @@ fn read_file(path: &str) -> Result<Vec<u8>, io::Error> {
     Ok(buffer)
 }
 
-fn dump_ttc<'a>(scope: ReadScope<'a>, ttc: TTCHeader<'a>) -> Result<(), ParseError> {
+fn dump_ttc<'a>(
+    scope: ReadScope<'a>,
+    ttc: TTCHeader<'a>,
+    tag: Option<Tag>,
+) -> Result<(), ParseError> {
     println!("TTC");
     println!(" - version: {}.{}", ttc.major_version, ttc.minor_version);
     println!(" - num_fonts: {}", ttc.offset_tables.len());
@@ -50,13 +96,21 @@ fn dump_ttc<'a>(scope: ReadScope<'a>, ttc: TTCHeader<'a>) -> Result<(), ParseErr
     for offset_table_offset in &ttc.offset_tables {
         let offset_table_offset = offset_table_offset as usize; // FIXME range
         let offset_table = scope.offset(offset_table_offset).read::<OffsetTable>()?;
-        dump_ttf(scope, offset_table)?;
+        dump_ttf(scope, offset_table, tag)?;
     }
     println!();
     Ok(())
 }
 
-fn dump_ttf<'a>(scope: ReadScope<'a>, ttf: OffsetTable<'a>) -> Result<(), ParseError> {
+fn dump_ttf<'a>(
+    scope: ReadScope<'a>,
+    ttf: OffsetTable<'a>,
+    tag: Option<Tag>,
+) -> Result<(), ParseError> {
+    if let Some(tag) = tag {
+        return dump_raw_table(tag, ttf.read_table(scope, tag)?).map_err(ParseError::from);
+    }
+
     println!("TTF");
     println!(" - version: 0x{:08x}", ttf.sfnt_version);
     println!(" - num_tables: {}", ttf.table_records.len());
@@ -78,7 +132,23 @@ fn dump_ttf<'a>(scope: ReadScope<'a>, ttf: OffsetTable<'a>) -> Result<(), ParseE
     Ok(())
 }
 
-fn dump_woff<'a>(scope: ReadScope<'a>, woff: WoffFile<'a>) -> Result<(), ParseError> {
+fn dump_woff<'a>(
+    scope: ReadScope<'a>,
+    woff: WoffFile<'a>,
+    tag: Option<Tag>,
+) -> Result<(), ParseError> {
+    if let Some(tag) = tag {
+        if let Some(entry) = woff.table_directory.iter().find(|entry| entry.tag == tag) {
+            let table = entry.read_table(woff.scope)?;
+
+            return dump_raw_table(tag, Some(table.scope())).map_err(ParseError::from);
+        } else {
+            eprintln!("Table {} not found", DisplayTag(tag));
+        }
+
+        return Ok(());
+    }
+
     println!("TTF in WOFF");
     println!(" - num_tables: {}\n", woff.table_directory.len());
 
@@ -112,7 +182,16 @@ fn dump_woff<'a>(scope: ReadScope<'a>, woff: WoffFile<'a>) -> Result<(), ParseEr
     Ok(())
 }
 
-fn dump_woff2<'a>(scope: ReadScope<'a>, woff: &Woff2File<'a>) -> Result<(), ParseError> {
+fn dump_woff2<'a>(
+    scope: ReadScope<'a>,
+    woff: &Woff2File<'a>,
+    tag: Option<Tag>,
+) -> Result<(), ParseError> {
+    if let Some(tag) = tag {
+        let table = woff.read_table(tag::NAME)?;
+        return dump_raw_table(tag, table.as_ref().map(|buf| buf.scope())).map_err(ParseError::from);
+    }
+
     println!("TTF in WOFF2");
     println!(" - num_tables: {}", woff.table_directory.len());
     println!(
@@ -197,6 +276,16 @@ fn dump_name_table(name_table: &NameTable) -> Result<(), ParseError> {
         }
         println!("{:?}", name);
         println!();
+    }
+
+    Ok(())
+}
+
+fn dump_raw_table(tag: Tag, scope: Option<ReadScope>) -> Result<(), io::Error> {
+    if let Some(scope) = scope {
+        io::stdout().write_all(scope.data())?;
+    } else {
+        eprintln!("Table {} not found", DisplayTag(tag));
     }
 
     Ok(())
