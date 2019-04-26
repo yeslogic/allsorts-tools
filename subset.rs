ca2b9b7becc3d287f76872f2523a26abedd27326
Wesley Moore <wes@wezm.net>
2019-04-26 13:50:46 +1000

    Tidy up subset binary

--- subset.rs
+++ subset.rs
@@ -1,14 +1,13 @@
-use encoding_rs::{Encoding, MACINTOSH, UTF_16BE};
 use getopts::Options;
 
 use fontcode::error::{ParseError, ReadWriteError};
 use fontcode::fontfile::FontFile;
 use fontcode::macroman;
 use fontcode::read::ReadScope;
-use fontcode::tables::{HeadTable, MaxpTable, NameTable, OffsetTable, OpenTypeFile, OpenTypeFont};
-use fontcode::tag::{self, DisplayTag};
+use fontcode::tables::{OffsetTable, OpenTypeFile, OpenTypeFont};
+use fontcode::tag;
 use fontcode::woff::WoffFile;
-use fontcode::woff2::{Woff2File, Woff2GlyfTable, Woff2LocaTable};
+use fontcode::woff2::Woff2File;
 
 use fontcode::glyph_index::read_cmap_subtable;
 use fontcode::gsub::{GlyphOrigin, RawGlyph};
@@ -90,15 +89,13 @@ fn subset_ttf<'a>(
     text: &str,
     output_path: &str,
 ) -> Result<(), Error> {
-    let script = tag::DFLT; // FIXME
-    let lang = tag::LATN; // FIXME
     let ttf = match &font_file.font {
         OpenTypeFont::Single(ttf) => ttf,
         OpenTypeFont::Collection(_ttc) => unimplemented!(),
     };
 
     // Work out the glyphs we want to keep from the text
-    let mut glyphs = chars_to_glyphs(font_file.scope, &ttf, script, lang, text)?;
+    let mut glyphs = chars_to_glyphs(font_file.scope, &ttf, text)?;
     let notdef = RawGlyph {
         unicodes: vec![],
         glyph_index: Some(0),
@@ -157,102 +154,17 @@ fn subset_ttf<'a>(
     Ok(())
 }
 
-fn subset_woff<'a>(scope: ReadScope<'a>, woff: WoffFile<'a>) -> Result<(), Error> {
-    println!("TTF in WOFF");
-    println!(" - num_tables: {}\n", woff.table_directory.len());
-
-    for entry in &woff.table_directory {
-        println!(
-            "{} (original checksum: 0x{:08x}, compressed length: {} original length: {})",
-            DisplayTag(entry.tag),
-            entry.orig_checksum,
-            entry.comp_length,
-            entry.orig_length
-        );
-        let _table = entry.read_table(scope)?;
-    }
-
-    let metadata = woff.extended_metadata()?;
-    if let Some(metadata) = metadata {
-        println!("\nExtended Metadata:\n{}", metadata);
-    }
-
-    println!();
-    if let Some(entry) = woff
-        .table_directory
-        .iter()
-        .find(|entry| entry.tag == tag::NAME)
-    {
-        let table = entry.read_table(woff.scope)?;
-        let name_table = table.scope().read::<NameTable>()?;
-        dump_name_table(&name_table)?;
-    }
-
-    Ok(())
+fn subset_woff<'a>(_scope: ReadScope<'a>, _woff: WoffFile<'a>) -> Result<(), Error> {
+    unimplemented!()
 }
 
-fn subset_woff2<'a>(scope: ReadScope<'a>, woff: &Woff2File<'a>) -> Result<(), Error> {
-    println!("TTF in WOFF2");
-    println!(" - num_tables: {}", woff.table_directory.len());
-    println!(
-        " - sizeof font data: {} compressed {} uncompressed\n",
-        woff.woff_header.total_compressed_size,
-        woff.table_data_block.len()
-    );
-
-    for entry in &woff.table_directory {
-        println!("{} {:?}", DisplayTag(entry.tag), entry,);
-    }
-
-    let metadata = woff.extended_metadata()?;
-    if let Some(metadata) = metadata {
-        println!("\nExtended Metadata:\n{}", metadata);
-    }
-
-    if let Some(entry) = woff.find_table_entry(tag::GLYF) {
-        println!();
-        let table = entry.read_table(scope)?;
-        let head = woff
-            .read_table(tag::HEAD)?
-            .ok_or(ParseError::BadValue)?
-            .scope()
-            .read::<HeadTable>()?;
-        let maxp = woff
-            .read_table(tag::MAXP)?
-            .ok_or(ParseError::BadValue)?
-            .scope()
-            .read::<MaxpTable>()?;
-        let loca_entry = woff
-            .find_table_entry(tag::LOCA)
-            .ok_or(ParseError::BadValue)?;
-        let loca = loca_entry.read_table(woff.table_data_block_scope())?;
-        let loca = loca.scope().read_dep::<Woff2LocaTable>((
-            &loca_entry,
-            usize::from(maxp.num_glyphs),
-            head.index_to_loc_format,
-        ))?;
-        let glyf = table.scope().read_dep::<Woff2GlyfTable>((&entry, loca))?;
-
-        println!("Read glyf table with {} glyphs:", glyf.records.len());
-        for glyph in glyf.records {
-            println!("- {:?}", glyph);
-        }
-    }
-
-    if let Some(table) = woff.read_table(tag::NAME)? {
-        println!();
-        let name_table = table.scope().read::<NameTable>()?;
-        dump_name_table(&name_table)?;
-    }
-
-    Ok(())
+fn subset_woff2<'a>(_scope: ReadScope<'a>, _woff: &Woff2File<'a>) -> Result<(), Error> {
+    unimplemented!()
 }
 
 fn chars_to_glyphs<'a>(
     scope: ReadScope<'a>,
     ttf: &OffsetTable<'a>,
-    _script: u32,
-    _lang: u32,
     text: &str,
 ) -> Result<Vec<Option<RawGlyph<()>>>, Error> {
     let cmap = ttf
@@ -291,82 +203,6 @@ fn make_glyph(ch: char, glyph_index: u16) -> RawGlyph<()> {
         fake_bold: false,
         fake_italic: false,
         extra_data: (),
-    }
-}
-
-fn dump_name_table(name_table: &NameTable) -> Result<(), ParseError> {
-    for name_record in &name_table.name_records {
-        let platform = name_record.platform_id;
-        let encoding = name_record.encoding_id;
-        let language = name_record.language_id;
-        let offset = usize::from(name_record.offset);
-        let length = usize::from(name_record.length);
-        let name_data = name_table
-            .string_storage
-            .offset_length(offset, length)?
-            .data();
-        let name = match (platform, encoding, language) {
-            (0, _, _) => decode(&UTF_16BE, name_data),
-            (1, 0, _) => decode(&MACINTOSH, name_data),
-            (3, 0, _) => decode(&UTF_16BE, name_data),
-            (3, 1, _) => decode(&UTF_16BE, name_data),
-            (3, 10, _) => decode(&UTF_16BE, name_data),
-            _ => format!(
-                "(unknown platform={} encoding={} language={})",
-                platform, encoding, language
-            ),
-        };
-        match get_name_meaning(name_record.name_id) {
-            Some(meaning) => println!("{}", meaning,),
-            None => println!("name {}", name_record.name_id,),
-        }
-        println!("{:?}", name);
-        println!();
-    }
-
-    Ok(())
-}
-
-fn decode(encoding: &'static Encoding, data: &[u8]) -> String {
-    let mut decoder = encoding.new_decoder();
-    if let Some(size) = decoder.max_utf8_buffer_length(data.len()) {
-        let mut s = String::with_capacity(size);
-        let (_res, _read, _repl) = decoder.decode_to_string(data, &mut s, true);
-        s
-    } else {
-        String::new() // can only happen if buffer is enormous
-    }
-}
-
-fn get_name_meaning(name_id: u16) -> Option<&'static str> {
-    match name_id {
-        0 => Some("Copyright"),
-        1 => Some("Font Family"),
-        2 => Some("Font Subfamily"),
-        3 => Some("Unique Identifier"),
-        4 => Some("Full Font Name"),
-        5 => Some("Version"),
-        6 => Some("PostScript Name"),
-        7 => Some("Trademark"),
-        8 => Some("Manufacturer"),
-        9 => Some("Designer"),
-        10 => Some("Description"),
-        11 => Some("URL Vendor"),
-        12 => Some("URL Designer"),
-        13 => Some("License Description"),
-        14 => Some("License Info URL"),
-        15 => None, // Reserved
-        16 => Some("Typographic Family"),
-        17 => Some("Typographic Subfamily"),
-        18 => Some("Compatible Full"),
-        19 => Some("Sample Text"),
-        20 => Some("PostScript CID findfont"),
-        21 => Some("WWS Family Name"),
-        22 => Some("WWS Subfamily Name"),
-        23 => Some("Light Background Palette"),
-        24 => Some("Dark Background Palette"),
-        25 => Some("Variations PostScript Name Prefix"),
-        _ => None,
     }
 }
 
