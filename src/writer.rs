b1a3deb3f30581035c41e0da76d982fbc2aad578
Wesley Moore <wes@wezm.net>
2025-11-11 14:34:51 +1000

    Update to handle more recent allsorts changes

--- src/writer.rs
+++ src/writer.rs
@@ -167,6 +167,7 @@ impl<'a, T> OutlineBuilder for NamedOutliner<'a, T>
 where
     T: OutlineBuilder,
 {
+    type Output = T::Output;
     type Error = T::Error;
 
     fn visit<V: OutlineSink>(
@@ -174,7 +175,7 @@ where
         glyph_index: u16,
         tuple: Option<&OwnedTuple>,
         visitor: &mut V,
-    ) -> Result<(), Self::Error> {
+    ) -> Result<Self::Output, Self::Error> {
         self.table.visit(glyph_index, tuple, visitor)
     }
 }
